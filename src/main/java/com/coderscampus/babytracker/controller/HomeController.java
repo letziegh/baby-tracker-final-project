205aae67713fd52a5368a66ae6d9be9b21dac4bc
letziegh <148257470+letziegh@users.noreply.github.com>
2025-01-06 12:17:26 -0500

    2nd commit m- Added oauth 2 security and Home Controller

---
+++ src/main/java/com/coderscampus/babytracker/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.coderscampus.babytracker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "Thank God you made it to the home page!";
+    }
+
+    @GetMapping("/secured")
+    public String secured() {
+        return "Hello, you are secured!";
+    }
+
+
+}
